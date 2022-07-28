99630087198519fdc97859c35b8f4389a4cf3776
Guillem <guillem.mart7@gmail.com>
2022-07-28 19:43:38 +0200

    delete exercise done

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,32 @@ import productData from './products.json';
 function App() {
   const [products, setProducts] = useState(productData);
 
+  function handleDelete(_id) {
+    const filtered = products.filter(product => product._id !== _id);
+    setProducts(filtered);
+  };
+
+  function handleSearch(e) {
+    if (e.target.value === "") {
+      setProducts(productData);
+    } else {
+      const filtered = products.filter(product => product.name.toLowerCase().includes(e.target.value));
+      setProducts(filtered);
+    }
+  };
+
   return (
     <div className="cart">
       <h1>My shopping cart</h1>
-
-      {/* Your code goes here */}
+      <input type="text" placeholder="Nice placeholder" onChange={e => handleSearch(e)}/>
+      {products.map(product => {
+        return (
+          <div className="eachProduct" key={product._id}>
+            <p>{product.name}</p>
+            <button className="delete-btn" onClick={() => handleDelete(product._id)}>Delete</button>
+          </div>
+        );
+      })}
 
     </div>
   );
